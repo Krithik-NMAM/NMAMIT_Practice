6e6b74c638536f59be58fff5818244da3e249569
Krithik K <132180690+Krithik-NMAM@users.noreply.github.com>
2023-08-04 12:09:58 +0530

    Update Vowels.c

--- Vowels.c
+++ Vowels.c
@@ -6,17 +6,19 @@ int main(){
     printf("Enter the word:\n");
     scanf("%s",word);
     printf("The word is:\n");
-    printf("%s\n",word);
+    printf("%s",word);
 
     n= strlen(word);
 
     printf("The required word is:\n");
     for(i=0;i<n;i++){
-        if((word[i]=='a')||(word[i]=='e')||(word[i]=='i')||(word[i]=='o')||(word[i]=='u')){
+        if((word[i]=='a')||(word[i]=='e')||(word[i]=='i')||(word[i]=='o')||(word[i]=='u')||(word[i]=='A')||(word[i]=='E')||(word[i]=='I')||(word[i]=='O')||(word[i]=='U')){
             continue;
         }
         printf("%c",word[i]);
     }
 
 }
+
+
 
